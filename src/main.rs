ffba5c3e2972a3e20f71dfdda31681f8bbccf241
ahabhgk <ahabhgk@gmail.com>
2021-08-26 11:57:55 +0800

    feat: vec3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod vec3;
+
+use vec3::Color;
+
 fn main() {
     let image_width = 256;
     let image_height = 256;
@@ -8,13 +12,9 @@ fn main() {
         for i in 0..image_width {
             let r = (i as f64) / ((image_width - 1) as f64);
             let g = (j as f64) / ((image_height - 1) as f64);
-            let b = 0.25f64;
-
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+            let b = 0.25;
+            let color = Color::new(r, g, b);
+            color.write();
         }
     }
 }
---
+++ src/vec3.rs
@@ -0,0 +1,169 @@
+use std::ops::{self, Deref};
+
+#[derive(Default, Clone, Copy)]
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn len_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn len(&self) -> f64 {
+        self.len_squared().sqrt()
+    }
+
+    pub fn dot(&self, v: &Self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn cross(&self, v: &Self) -> Self {
+        Self {
+            x: self.y * v.z - self.z * v.y,
+            y: self.z * v.x - self.x * v.z,
+            z: self.x * v.y - self.y * v.x,
+        }
+    }
+
+    pub fn unit(self) -> Self {
+        self / Vec3::from(self.len())
+    }
+}
+
+impl From<f64> for Vec3 {
+    fn from(num: f64) -> Self {
+        Self {
+            x: num,
+            y: num,
+            z: num,
+        }
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl ops::Div for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::Output {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+        }
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self::Output {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl ops::SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl ops::MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+
+impl ops::DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        self.x /= rhs.x;
+        self.y /= rhs.y;
+        self.z /= rhs.z;
+    }
+}
+
+pub type Point = Vec3;
+
+#[derive(Default)]
+pub struct Color(Vec3);
+
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Self {
+        Self(Vec3::new(r, g, b))
+    }
+
+    pub fn write(&self) {
+        let num = 255.999;
+        println!(
+            "{} {} {}",
+            (num * self.x) as i32,
+            (num * self.y) as i32,
+            (num * self.z) as i32
+        );
+    }
+}
+
+impl Deref for Color {
+    type Target = Vec3;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
